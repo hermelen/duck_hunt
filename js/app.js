6411016f40080a7d2f7e50e9862bac87c9040c8b
zampiello alexia <alexzamp@hotmail.fr>
2018-06-14 15:50:37 +0200

    mise en place du left et du random

--- js/app.js
+++ js/app.js
@@ -5,14 +5,25 @@ duck.className = 'duck';
 var main = document.getElementsByTagName('main')[0];
 main.appendChild(duck);
 
-topVal = duck.style.top = "200px";
-leftVal = duck.style.left = "20px";
+topVal = duck.style.top = "400px";
+leftVal = duck.style.left = "500px";
 
 window.setInterval(function(){
-  topVal = topVal.replace('px', '');
-  topVal = parseInt(topVal);
-  topVal = topVal-5;
+  topRandom = getRandomInt(-100,100);
+  topVal = topVal.replace('px', '');// ="200"
+  topVal = parseInt(topVal);// = 200
+  topVal = topVal+topRandom;
   topVal = topVal + "px";
   console.log(topVal);
   duck.style.top = topVal;
+  leftRandom = getRandomInt(-100,100);
+  leftVal = leftVal.replace('px', '');
+  leftVal = parseInt(leftVal);
+  leftVal = leftVal+leftRandom;
+  leftVal = leftVal + "px";
+  duck.style.left = leftVal;
 },1000);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
